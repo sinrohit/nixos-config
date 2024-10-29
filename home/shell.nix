20f71acd1a00f3ead1ebf37920f66a7abf86f3ae
Rohit Singh <rsrohitsingh682@gmail.com>
2024-10-29 09:41:01 +0530

    shell: Export MyScripts to path

--- home/shell.nix
+++ home/shell.nix
@@ -17,6 +17,7 @@
       envExtra = ''
         # Make Nix and home-manager installed things available in PATH.
         export PATH=/run/wrappers/bin:/run/current-system/sw/bin/:/nix/var/nix/profiles/default/bin:$HOME/.nix-profile/bin:/etc/profiles/per-user/$USER/bin:$PATH
+        export PATH="/Users/rohit.singh/MyScripts/:$PATH" 
         eval "$(zoxide init --cmd cd zsh)"
       '';
     };
