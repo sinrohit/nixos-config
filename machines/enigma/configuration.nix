a85232621e9035816852b15d5ac5fb69a36f20dd
Rohit Singh <rsrohitsingh682@gmail.com>
2024-10-29 09:41:16 +0530

    nix: fix network manager warning

--- machines/enigma/configuration.nix
+++ machines/enigma/configuration.nix
@@ -36,6 +36,10 @@ in
 
   nix.settings.trusted-users = [ "root" ] ++ lib.attrNames users;
 
+  # Workaround the annoying `Failed to start Network Manager Wait Online` error on switch.
+  # https://github.com/NixOS/nixpkgs/issues/180175
+  systemd.services.NetworkManager-wait-online.enable = false;
+
   home-manager.users =
     lib.flip lib.mapAttrs users (name: cfg: {
       imports = [
