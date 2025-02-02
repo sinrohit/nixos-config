703a1df6587b93beab248b715fb4bd19c18b7853
Rohit Singh <rsrohitsingh682@gmail.com>
2025-02-02 17:23:00 +0530

    secrets: add new public key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,5 +1,5 @@
 let
-  rohit = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEvCnbNCc22/DeR7cZVUHv3PwwfpL6kIAHO4Ns7SMj1h";
+  rohit = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOWUINFSlhsbwckdAlvE/V1ESIk0yXdVvE/BuMEJpEvl";
   users = [ rohit ];
   
   pi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH7XfzR41VnfOP4Dh6Jxkew6Jz1BA9zAaXZMieAC9BSp";
