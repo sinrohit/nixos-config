908b38bf219619f8a501fb7dd159fc832178455e
Rohit Singh <rsrohitsingh682@gmail.com>
2025-02-07 14:07:21 +0530

    sshd: add new ssh key

--- machines/pi/configuration.nix
+++ machines/pi/configuration.nix
@@ -110,7 +110,7 @@
       isNormalUser = true;
       extraGroups = [ "wheel" "docker" "networkmanager" ];
       openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH7XfzR41VnfOP4Dh6Jxkew6Jz1BA9zAaXZMieAC9BSp"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOWUINFSlhsbwckdAlvE/V1ESIk0yXdVvE/BuMEJpEvl"
       ];
     };
     users.nginx.extraGroups = [ "acme" "disk" ];
