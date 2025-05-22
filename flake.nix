797af365bb0c261cf125f9ba66f61b3ea06457a5
Rohit Singh <rsrohitsingh682@gmail.com>
2025-05-22 19:48:46 +0530

    refactor: get rid of nixos-unified and extra things

--- flake.nix
+++ flake.nix
@@ -1,95 +1,74 @@
 {
+  description = "NixOS systems";
+
   inputs = {
 
-    # Principle inputs (updated by `nix run .#update`)
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    nixos-unified.url = "github:srid/nixos-unified";
-    systems.url = "github:nix-systems/default";
-    nixos-hardware.url = "github:nixos/nixos-hardware";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
 
-    # Software inputs
-    nix-index-database.url = "github:nix-community/nix-index-database";
-    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
-    nvnix.url = "github:rsrohitsingh682/nvnix";
-    nvnix.inputs.nixpkgs.follows = "nixpkgs";
+    # Used to get ibus 1.5.29 which has some quirks we want to test.
+    nixpkgs-old-ibus.url = "github:nixos/nixpkgs/e2dd4e18cc1c7314e24154331bae07df76eb582f";
+
+    # We use the unstable nixpkgs repo for some packages.
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-darwin = {
+      url = "github:nix-darwin/nix-darwin/nix-darwin-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
     ragenix.url = "github:yaxitech/ragenix";
 
-    omnix.url = "github:juspay/omnix";
-
-    # Devshell
-    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
-  outputs = inputs@{ self, ... }:
+  outputs =
+    inputs@{ self, ... }:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = import inputs.systems;
-      imports = [
-        inputs.nixos-unified.flakeModule
-        inputs.treefmt-nix.flakeModule
-        ./config.nix
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
       ];
+      imports = [ ./lib/mksystem.nix ];
 
-      perSystem = { pkgs, self', config, inputs', ... }: {
+      perSystem =
+        {
+          pkgs,
+          inputs',
+          ...
+        }:
+        {
 
-        treefmt.config = {
-          projectRootFile = "flake.nix";
-          programs.nixpkgs-fmt.enable = true;
+          devShells.default = pkgs.mkShell {
+            name = "nixos-config-shell";
+            meta.description = "Shell environment for modifying this Nix configuration";
+            packages = with pkgs; [
+              nixd
+              nil
+              inputs'.ragenix.packages.default
+            ];
+          };
+
         };
-
-        devShells.default = pkgs.mkShell {
-          name = "nixos-config-shell";
-          meta.description = "Shell environment for modifying this Nix configuration";
-          packages = with pkgs; [
-            just
-            nixd
-            inputs'.ragenix.packages.default
-          ];
-        };
-
-        packages.default = self'.packages.activate; # Enables `nix run`
-
-      };
-
-      flake.checks.x86_64-linux = {
-        enigma = self.nixosConfigurations.enigma.config.system.build.toplevel;
-
-        pi = self.nixosConfigurations.pi.config.system.build.toplevel;
-
-        riscv = self.nixosConfigurations.riscv.config.system.build.toplevel;
-      };
 
       flake = {
-        # Configurations for Linux (NixOS) machines
         nixosConfigurations = {
-          enigma = self.nixos-unified.lib.mkLinuxSystem { home-manager = true; } {
-            imports = [ ./machines/enigma/configuration.nix ];
-            nixos-unified.sshTarget = "enigma";
-          };
-
-          pi = self.nixos-unified.lib.mkLinuxSystem { home-manager = false; } {
-            imports = [
-              ./machines/pi/configuration.nix
-              inputs.ragenix.nixosModules.default
-            ];
-            nixos-unified.sshTarget = "pi";
-          };
-
-          riscv = self.nixos-unified.lib.mkLinuxSystem { home-manager = false; } {
-            imports = [
-              ./machines/riscv/configuration.nix
-            ];
-            nixos-unified.sshTarget = "riscv";
-          };
-
+          ema = self.mkSystem.nixOS "ema" { withHomeManager = true; };
+          pi = self.mkSystem.nixOS "pi";
         };
 
-        darwinConfigurations.zion = self.nixos-unified.lib.mkMacosSystem { home-manager = true; } {
-          imports = [ ./machines/zion/configuration.nix ];
+        darwinConfigurations.zion = self.mkSystem.macOS "zion" { withHomeManager = true; };
+
+        checks.x86_64-linux = {
+
+          ema = self.nixosConfigurations.ema.config.system.build.toplevel;
+
+          pi = self.nixosConfigurations.pi.config.system.build.toplevel;
         };
       };
     };
---
+++ lib/mksystem.nix
@@ -0,0 +1,121 @@
+{
+  self,
+  lib,
+  inputs,
+  ...
+}:
+
+let
+  specialArgs = { inherit self inputs; };
+
+  nixosModules = {
+    home-manager = {
+      imports = [
+        inputs.home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = specialArgs;
+          home-manager.users."rohit" = import ../home;
+        }
+      ];
+    };
+  };
+  # Common and useful setting across all platforms
+  common =
+    { lib, ... }:
+    {
+      nix = {
+        settings = {
+          # Use all CPU cores
+          max-jobs = lib.mkDefault "auto";
+          experimental-features = lib.mkDefault "nix-command flakes";
+          substituters = [ "https://cache.clan.lol" ];
+          trusted-public-keys = [ "cache.clan.lol-1:3KztgSAB5R1M+Dz7vzkBGzXdodizbgLXGXKXlcQLA28=" ];
+        };
+      };
+    };
+
+  homeModules = {
+    common =
+      { config, pkgs, ... }:
+      {
+        # Sensible default for `home.homeDirectory`
+        home.homeDirectory = lib.mkDefault "/${
+          if pkgs.stdenv.isDarwin then "Users" else "home"
+        }/${config.home.username}";
+
+        # For macOS, $PATH must contain these.
+        home.sessionPath = lib.mkIf pkgs.stdenv.isDarwin [
+          "/etc/profiles/per-user/$USER/bin" # To access home-manager binaries
+          "/nix/var/nix/profiles/system/sw/bin" # To access nix-darwin binaries
+          "/usr/local/bin" # Some macOS GUI programs install here
+        ];
+      };
+  };
+  darwinModules = {
+    home-manager = {
+      imports = [
+        inputs.home-manager.darwinModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.sharedModules = [ homeModules.common ];
+          home-manager.extraSpecialArgs = specialArgs;
+          home-manager.users."rohit" = import ../home;
+        }
+      ];
+    };
+  };
+in
+{
+  config.flake.mkSystem = {
+    nixOS =
+      hostName:
+      {
+        withHomeManager ? false,
+      }:
+      inputs.nixpkgs.lib.nixosSystem {
+        inherit specialArgs;
+        modules = [
+          # Apply our overlays. Overlays are keyed by system type so we have
+          # to go through and apply our system type. We do this first so
+          # the overlays are available globally.
+          {
+            nixpkgs.overlays = [ (import ../overlays { inherit inputs; }) ];
+          }
+          common
+
+          # Allow unfree packages.
+          { nixpkgs.config.allowUnfree = true; }
+
+          ../machines/${hostName}
+          ../users/nixos.nix
+        ] ++ lib.optional withHomeManager nixosModules.home-manager;
+      };
+
+    macOS =
+      hostName:
+      {
+        withHomeManager ? false,
+      }:
+      inputs.nix-darwin.lib.darwinSystem {
+        inherit specialArgs;
+        modules = [
+          # Apply our overlays. Overlays are keyed by system type so we have
+          # to go through and apply our system type. We do this first so
+          # the overlays are available globally.
+          {
+            nixpkgs.overlays = [ (import ../overlays { inherit inputs; }) ];
+          }
+          common
+
+          # Allow unfree packages.
+          { nixpkgs.config.allowUnfree = true; }
+
+          ../machines/${hostName}
+          #../users/darwin.nix
+        ] ++ lib.optional withHomeManager darwinModules.home-manager;
+      };
+  };
+}
