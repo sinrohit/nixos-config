b5f05499b67c63776f160d2d59a86254095157ac
Rohit Singh <rsrohitsingh682@gmail.com>
2025-05-23 16:25:06 +0530

    pi: fix config

--- flake.nix
+++ flake.nix
@@ -59,7 +59,7 @@
       flake = {
         nixosConfigurations = {
           ema = self.mkSystem.nixOS "ema" { withHomeManager = true; };
-          pi = self.mkSystem.nixOS "pi";
+          pi = self.mkSystem.nixOS "pi" { };
         };
 
         darwinConfigurations.zion = self.mkSystem.macOS "zion" { withHomeManager = true; };
