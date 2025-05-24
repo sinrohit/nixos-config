a44206889cabc89fce26cb76509d1b6fb41d6d46
Rohit Singh <rsrohitsingh682@gmail.com>
2025-05-24 16:14:42 +0530

    helix: add more config

---
+++ home/helix.nix
@@ -0,0 +1,67 @@
+{ pkgs, lib, ... }:
+{
+  programs.helix = {
+    enable = true;
+
+    languages.language = [
+      {
+        name = "markdown";
+        soft-wrap.enable = true;
+        soft-wrap.wrap-indicator = "";
+        soft-wrap.wrap-at-text-width = true;
+      }
+      {
+        name = "nix";
+        auto-format = true;
+        formatter.command = lib.getExe pkgs.nixfmt-rfc-style;
+        language-servers = [
+          "nixd"
+          "nil"
+        ];
+      }
+      {
+        name = "haskell";
+        auto-format = true;
+        formatter.command = lib.getExe pkgs.fourmolu;
+        language-servers = [
+          "hls"
+        ];
+      }
+      {
+        name = "rust";
+        auto-format = true;
+        formatter.command = lib.getExe pkgs.rustfmt;
+        language-servers = [
+          "rust-analyzer"
+        ];
+      }
+    ];
+
+    languages.language-server = {
+      nixd.command = lib.getExe pkgs.nixd;
+      nil.command = lib.getExe pkgs.nil;
+      hls.command = lib.getExe pkgs.haskell-language-server;
+      rust-analyzer = lib.getExe pkgs.rust-rust-analyzer;
+    };
+
+    settings = {
+      theme = "gruvbox";
+      editor = {
+        # diagnostics
+        end-of-line-diagnostics = "hint";
+        inline-diagnostics.cursor-line = "error";
+
+        bufferline = "multiple";
+        color-modes = true;
+        completion-replace = true;
+        completion-timeout = 5;
+        completion-trigger-len = 1;
+        cursorline = true;
+        cursor-shape.insert = "bar";
+        file-picker.hidden = false;
+        lsp.display-inlay-hints = true;
+        preview-completion-insert = false;
+      };
+    };
+  };
+}
--- home/packages.nix
+++ home/packages.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 {
 
   programs = {
@@ -22,58 +22,6 @@
       settings = {
         show_tabs = false;
         style = "compact";
-      };
-    };
-
-    helix = {
-      enable = true;
-
-      languages.language = [
-        {
-          name = "git-commit";
-          rulers = [ 80 ];
-        }
-        {
-          name = "markdown";
-          soft-wrap.enable = true;
-          soft-wrap.wrap-indicator = "";
-          soft-wrap.wrap-at-text-width = true;
-        }
-        {
-          name = "nix";
-          auto-format = true;
-          formatter.command = lib.getExe pkgs.nixfmt-rfc-style;
-          language-servers = [
-            "nixd"
-            "nil"
-          ];
-        }
-      ];
-
-      languages.language-server = {
-        nixd.command = lib.getExe pkgs.nixd;
-        nil.command = lib.getExe pkgs.nil;
-      };
-
-      settings = {
-        theme = "gruvbox";
-        editor = {
-          # diagnostics
-          end-of-line-diagnostics = "hint";
-          inline-diagnostics.cursor-line = "error";
-
-          bufferline = "multiple";
-          color-modes = true;
-          completion-replace = true;
-          completion-timeout = 5;
-          completion-trigger-len = 1;
-          cursorline = true;
-          cursor-shape.insert = "bar";
-          file-picker.hidden = false;
-          lsp.display-inlay-hints = true;
-          preview-completion-insert = false;
-        };
-
       };
     };
   };
