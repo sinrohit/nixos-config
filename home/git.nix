5041816c1bb003ff02294248318ea482ce2bef80
Rohit Singh <rsrohitsingh682@gmail.com>
2026-01-03 19:23:42 +0530

    ema: fixes due to upgrade

--- home/git.nix
+++ home/git.nix
@@ -2,21 +2,23 @@
 
   programs.git = {
     enable = true;
-    userName = "Rohit Singh";
-    userEmail = "rsrohitsingh682@gmail.com";
-    aliases = {
-      cleanup = "!git branch --merged | grep  -v '\\*\\|master\\|develop' | xargs -n 1 -r git branch -d";
-      prettylog = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(r) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
-      root = "rev-parse --show-toplevel";
-    };
-    extraConfig = {
-      branch.autosetuprebase = "always";
-      color.ui = true;
-      core.askPass = ""; # needs to be empty to use terminal for ask pass
-      credential.helper = "store"; # want to make this more secure
-      github.user = "sinrohit";
-      push.default = "tracking";
-      init.defaultBranch = "main";
+    settings = {
+      user.name = "Rohit Singh";
+      user.email = "rsrohitsingh682@gmail.com";
+      aliases = {
+        cleanup = "!git branch --merged | grep  -v '\\*\\|master\\|develop' | xargs -n 1 -r git branch -d";
+        prettylog = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(r) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
+        root = "rev-parse --show-toplevel";
+      };
+      extraConfig = {
+        branch.autosetuprebase = "always";
+        color.ui = true;
+        core.askPass = ""; # needs to be empty to use terminal for ask pass
+        credential.helper = "store"; # want to make this more secure
+        github.user = "sinrohit";
+        push.default = "tracking";
+        init.defaultBranch = "main";
+      };
     };
   };
 }
