c8c3a56b378be40dd3f638262ebf05476e82a56e
Rohit Singh <rsrohitsingh682@gmail.com>
2026-01-20 00:17:45 +0530

    cache: add `sinrohit.cachix.org`

--- lib/common.nix
+++ lib/common.nix
@@ -10,9 +10,13 @@
       # Use all CPU cores
       max-jobs = lib.mkDefault "auto";
       experimental-features = lib.mkDefault "nix-command flakes";
-      extra-substituters = [ "https://cache.clan.lol" ];
+      extra-substituters = [
+        "https://cache.clan.lol"
+        "https://sinrohit.cachix.org"
+      ];
       extra-trusted-public-keys = [
         "cache.clan.lol-1:3KztgSAB5R1M+Dz7vzkBGzXdodizbgLXGXKXlcQLA28="
+        "sinrohit.cachix.org-1:EwMRl5uQRWFvuUX+/61+bkFpUOl877ToztsmJ+N6KiY="
       ];
     };
   };
