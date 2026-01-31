110cdc6767bbcf6644dc856498e80e27ff87cb47
Rohit Singh <rsrohitsingh682@gmail.com>
2026-01-31 21:06:56 +0530

    orb: get rid of it.

--- modules/flake/configurations.nix
+++ modules/flake/configurations.nix
@@ -6,7 +6,6 @@
       ema = self.mkSystem.nixOS "ema" { withHomeManager = true; };
       enigma = self.mkSystem.nixOS "enigma" { withHomeManager = true; };
       pi = self.mkSystem.nixOS "pi" { };
-      orb = self.mkSystem.nixOS "orb" { withHomeManager = true; };
       riscv = self.mkSystem.nixOS "riscv" { };
     };
 
