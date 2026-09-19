033f0452c8f96cc1ed109868f0a042ad408c47d3
Rohit Singh <rsrohitsingh682@gmail.com>
2026-09-19 18:57:16 +0530

    caddy: update hash

--- modules/nixos/services/caddy.nix
+++ modules/nixos/services/caddy.nix
@@ -50,7 +50,7 @@ in
 
       package = pkgs.caddy.withPlugins {
         plugins = [ "github.com/caddy-dns/cloudflare@v0.2.4" ];
-        hash = "sha256-hEHgAG0F0ozHRAPuxEqLyTATBrE+pajeXDiSNwniorg=";
+        hash = "sha256-dQvk6ezY6TQ1J7PjhCXnThF/SqVgPwBO8/RXzHCY+js=";
       };
 
       # Global options block — sets ACME defaults for all vhosts
